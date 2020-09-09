3f556c49a33fab56a7c2ab14094f1a6ece32939e
Paul Mason <paul.mason@experian.com>
2020-09-09 23:25:52 +0100

    remove emails

--- src/functions/emailer.js
+++ src/functions/emailer.js
@@ -1,26 +1,26 @@
-import nodemailer from 'nodemailer';
+// import nodemailer from 'nodemailer';
 
-exports.handler = function (event, context, callback) {
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'chiefspammer@yourgreatdomain.com',
-      pass: 'SuperSecretPassword', // naturally, replace both with your real credentials or an application-specific password
-    },
-  });
+// exports.handler = function (event, context, callback) {
+//   const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//       user: 'chiefspammer@yourgreatdomain.com',
+//       pass: 'SuperSecretPassword', // naturally, replace both with your real credentials or an application-specific password
+//     },
+//   });
 
-  const mailOptions = {
-    from: 'barry@baz.com',
-    to: 'paul.imperialroad@gmail.com',
-    subject: 'Test',
-    text: 'This is a test',
-  };
+//   const mailOptions = {
+//     from: 'barry@baz.com',
+//     to: 'paul.imperialroad@gmail.com',
+//     subject: 'Test',
+//     text: 'This is a test',
+//   };
 
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });
-};
+//   transporter.sendMail(mailOptions, function (error, info) {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log('Email sent: ' + info.response);
+//     }
+//   });
+// };
