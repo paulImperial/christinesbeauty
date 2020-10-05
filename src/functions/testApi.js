97ef17d93db41d474dd84b39899959c9f165c5c1
Paul Mason <paul.mason@experian.com>
2020-10-05 17:14:53 +0100

    add data to trestapi call

--- src/functions/testApi.js
+++ src/functions/testApi.js
@@ -1,11 +1,6 @@
 exports.handler = function (event, context, callback) {
-  const data = {
-    name: 'bob',
-    age: 35,
-  };
-
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify(data),
+    body: JSON.stringify({ name: 'fred' }),
   });
 };
