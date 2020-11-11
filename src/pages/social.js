39abaf81825004a40ec7bde9d8b5619db81acfb8
Paul Mason <paul.mason@experian.com>
2020-11-11 18:44:32 +0000

    npm audit

--- src/pages/social.js
+++ src/pages/social.js
@@ -5,20 +5,8 @@ import Container from '../components/Generic/Container';
 import Social from '../components/Social/Social';
 import Router from 'next/router';
 
-const BodyText = styled.p`
-  margin: 10px 0;
-  padding: 5px;
-  font-size: 1.25rem;
-`;
-
 const socialPage = () => {
   const title = `Christine's Beauty | Social Hub`;
-
-  const reloadSocial = () => {
-    if (process.browser) {
-      Router.reload(window.location.pathname);
-    }
-  };
 
   return (
     <Layout title={title}>
