2725396af9062d93e2251c8e6bea7f8a09a46299
Paul Mason <paul.mason@experian.com>
2020-11-12 17:45:15 +0000

    change url

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -92,7 +92,7 @@ const Form = () => {
     const data = JSON.stringify({ name, email, phone, advert, message });
 
     try {
-      const response = await fetch('https://christines-beauty.netlify.app/.netlify/functions/emailer', {
+      const response = await fetch('https://christinesbeauty.co.uk/.netlify/functions/emailer', {
         method: 'POST',
         body: data,
         mode: 'no-cors',
