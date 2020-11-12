06242fd87a93257aaee8bc014ebda8bc74ff5f17
Paul Mason <paul.mason@experian.com>
2020-11-12 18:10:59 +0000

    remove no cors

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -95,7 +95,6 @@ const Form = () => {
       const response = await fetch('https://christinesbeauty.co.uk/.netlify/functions/emailer', {
         method: 'POST',
         body: data,
-        mode: 'no-cors',
       });
 
       if (response.status === 200) {
